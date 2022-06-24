a30dd5d756f90311e58d694ea947da7859de9e6c
Muzamilkamran <muzamilkamran3@gmail.com>
2022-06-24 08:52:05 -0700

    varible or comnts

---
+++ tut4.cpp
@@ -0,0 +1,13 @@
+                            Today we study
+high level agar me google kuh search krta hn wo wo show krta ha wo high level ha or 
+agr me is k andar jay gy tu low level ha                             
+low level id nesr your harddisk k pass ha wo low level
+coments/ men ne int main(){
+    //his is creating for storing the area
+    /*a=1
+    b=3
+    c=4\*
+}
+variables\ is container to store data for exmple int,_1,01 int me hoty __hn
+ float/0.1 like that in float,
+ char/a,b,c
