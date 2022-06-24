bcd29cfa1171259a1a6e078e27122024ebc366db
Muzamilkamran <muzamilkamran3@gmail.com>
2022-06-24 09:05:59 -0700

    uses of coments&variable

---
+++ tut4part2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+// This program was created by muzamil kamran
+/*this 
+is 
+multi
+line 
+coment*/
+int main(){
+    int sum=6;
+    cout<<"Helo world"<< sum;
+    return 0;
+}
