ac00cb0a23d7b1cfc357419f551d4cb98de5f6a4
Muzamilkamran <muzamilkamran3@gmail.com>
2022-06-24 05:42:56 -0700

    badsic struture of prgming

---
+++ tut3.cpp
@@ -0,0 +1,15 @@
+c++ lang make in 1979 by bbjarne stroustrup
+whyn we use c++ lang because make fast program more control over system resoruce 
++ memory mangnent
+c++ make us high perfoms
+major upadate 
+2011_c++11
+2014_c++14
+2017_c++17
+#include<iostream> this is herdfile it's mean iostream file is also avable preper it's function
+io mean input and output to help us 
+after #include <iostream> no effect how many line u leave 
+int mean intrial and main is functin 
+<< ""double mein koi bhi cheez inter kry gy wo  string operate hai is mein computer ko 
+khe rha hun helo world print kr do srcean per
+return ki 0 value ka mtlb hai successful terimnation program
