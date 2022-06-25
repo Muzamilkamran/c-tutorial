ba993b7a26a9e2cc0008f9f7d5f25d74645cc55e
Muzamilkamran <muzamilkamran3@gmail.com>
2022-06-25 05:31:21 -0700

    kuh smj aye kuh nhi phr try kr k updata krta hn

---
+++ tut5part2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+int glo=6;
+void sum(){
+    int a;
+}
+int main(){
+// int a=14;
+// int b=17;
+int a=14, b=17;
+float pi=3.17;
+char c ='u';
+bool is-ture=ture
+sum();
+cout<<glo<<is-ture;
+//     // cout<<"This is tutorial 5. \nHere the value of a is "<<a<<". \nThe value of b is "<< b;
+//     // cout<<"\nthe val of pi is: "<<pi;
+//     // cout<<"\nthe val of c is: "<<c;
+}
+
