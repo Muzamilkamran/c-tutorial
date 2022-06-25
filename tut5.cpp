4e8785194059cfe14b2c671ed6dc602cb1569497
Muzamilkamran <muzamilkamran3@gmail.com>
2022-06-25 04:05:22 -0700

    varible data types etc

---
+++ tut5.cpp
@@ -0,0 +1,26 @@
+variable scope & data type 
+variablea is container data hold krne k liye 
+variable can be of various type 
+int:: men is 1,2,3,7,0,
+floT ::men aty hen1.21,3.75 
+char::men aty hen a,b,c
+double ::men aty  hen 1.21153 like men thra zyda no use hoty hn point k baad 
+Boolean::men aty hen agr humne ne female or male or yes or no ka ans use kry gy tu boolean ka 
+formula use kry gy 
+int sum=34;mean sum  is integer value which is hold value 34 in memory
+data type and variable name=value 
+ex- a=2,b=6;
+Baseed on scope, variable can be classifed onto types:
+1.local variable 
+2.golbal variable 
+socpe of variable is region in code where the  variable of existence is valid
+variable have two types local variable &Global variable
+local variable kis b ek k function andar declear krty  me use hoty hen 
+global variable ki b function k bhir use hoty hen 
+we can make global and local variabke use same how? yet to com 
+Data types define the type of data a variable can hold for exmpl;integer variable variable can hold 
+integer data , a chacater type of data varible can hold char data etc.
+there are three types of data 
+1.bulit-in------is men integer,chacater,float,boolean,double,
+2.user-defined type as ,struct,union,enum, 
+3.Derived tpes as ---Array,Function,pointer,
