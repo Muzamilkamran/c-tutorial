75aeb93876a7d195ebdf24e4bacc1cd7eb7214e6
Muzamilkamran <muzamilkamran3@gmail.com>
2022-06-28 05:45:42 -0700

    basic input or output in c++

---
+++ tut5part3.cpp
@@ -0,0 +1,7 @@
+c++ user input or output or more 
+c++ comes with libraries which help us in performing input output 
+what is mean by stream 
+ans= In c++ sequence of bytes correponding to input or output are commonly known as stream
+input stream=direction of flow of bytes take place input device (for ex-keyboard) to main memory.
+output steream= direction of flow of bytes take place from main memory to output device.
+for ex---display is output  
