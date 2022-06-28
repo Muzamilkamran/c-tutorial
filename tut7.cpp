d7994b6e4fa21e76ded3ad375476dfc3f321be1a
Muzamilkamran <muzamilkamran3@gmail.com>
2022-06-28 06:48:35 -0700

    Header files

---
+++ tut7.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+// There are two types of heder files:
+// 1. System header fils: Its come with the compiler files:
+// 2. User defined header files: Its written by the programmer
+// #include "this.h"
+using namespace std;
+int main(){
+    cout<<"This is helo world program";
+
+    return 0;
+}
