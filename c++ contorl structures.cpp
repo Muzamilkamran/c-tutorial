d676bdb86c57d801bdaffd4d117ba5e9cda9dd3f
Muzamilkamran <muzamilkamran3@gmail.com>
2022-06-29 05:36:29 -0700

    c++ contorl structures

---
+++ c++ contorl structures.cpp
@@ -0,0 +1,15 @@
+// there are three types of structures
+// 1.sequence structure
+// 2.selection structures
+// 3.loop structures
+// basic contorl structures
+// sequene structures--> sab sy phly apne enter li or apne kha action 1 or 2 ko kr esi trh is krty hn 
+// hum apna function compelet krty __hen 
+// 2.selection structures--> is men sb ohly ap enter li phr ap sawal krty for emple 
+// ap p6y hen k age apki 18 hen ya nhi jb ture krty ye ek sawal ka selection krty hen is trh hota ha or 
+// final exit exit lety hen 
+// 3.loop structures--->is ap enter krty hen loop ane k bad agr ture ha tu action krty hen ap waps loop lty hen  
+// kiya i ki value 10 sy kamm true krty less or gather than jesy hi false ho ga action hity hi exit krn ga 
+// if else statment
+
+
