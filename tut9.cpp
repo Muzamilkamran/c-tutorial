14b5b89d48a8e06dd2b320b94cc0bb07d379ec64
Muzamilkamran <muzamilkamran3@gmail.com>
2022-06-29 06:18:51 -0700

    sequence contorl structures

---
+++ tut9.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+cout<<"tell me your age";
+cin>>age;
+// selection contorl structures if else if else or letter 
+// if((age<18) && (age>0)){
+// cout<<"you can not go to smoke"<<endl;
+// }
+// else if(age==18){
+//     cout<<"take permisson to your family"<<endl;
+// }
+// else if(age<1){
+//     cout<<"you are not born yet"<<endl;
+// }
+// else{
+// cout<<"you can smoke"<<endl;
+// }
+// selection control structures switch case 
+
+}
+
+
+
+    return 0;
+}
