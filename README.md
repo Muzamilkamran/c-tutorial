d4416eb24a0a962444f5eac88cb5a3491715c882
Muzamilkamran <muzamilkamran3@gmail.com>
2022-06-30 05:14:17 -0700

    C++ Multiplication Table using While loop | Beginners | Step By Step | Engineering Tutorials 4 you

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# c-tutorial
+# c++ tutorial
+  - C++ Multiplication Table using While loop | Beginners | Step By Step | Engineering Tutorials 4 you
+
+
---
+++ tut10.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // lopp in c++:
+    // there are three types of loop in c++
+    // 1.for loop
+    // 2.while loop
+    // 3.do- while loop
+    // int i=1;
+    // cout<<i;
+    // i++;
+    // syntax for for loop
+    // for((initiazliation;-->int i=0; int this terms)+
+    // (condition;--->cout<<i<<endl;in this terms )+(updation;--->i++ in this terms ))
+    // {
+    // Body loop(c++ code)
+    // }
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     /* code */
+    //     cout<<i<<endl;
+    // }
+    // FOR example of infinite loop
+    // for (int i = 0; 34 < 4; i++)
+    // {
+    //     /* code */
+    //     cout<<i<<endl;
+    // }
+    /*while loop in c++*/
+    // syntax:
+    // while(condition):
+    // {
+    //  c++ statements;
+    // }
+    // printing 1 to 40 using while loop
+    // int i=1;
+    // while (i<=40){
+    //     cout<<i<<endl;
+    //     i++;
+    // }
+    // {
+    //     /* code */
+    // }
+    // FOR example of infinite loop
+    // int i=1;
+    // while(true){
+    //     cout<<i<<endl;
+    //     i++;
+
+    // }
+    /*while loop in c++*/
+    // syntax:
+    // do:
+    // {
+    //  c++ statements;
+    // }while (condition)
+ int i=1;
+    do{
+        cout<<i<<endl;
+        i++;
+    }while (i<=40);
+    return 0;
+}
