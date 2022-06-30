5cd0a4ab18cff676f2ad8290029bf71ea35729f3
Muzamilkamran <muzamilkamran3@gmail.com>
2022-06-30 06:54:36 -0700

    break & contiune

---
+++ tut11.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+// int main(){
+// for (int i = 0; i < 40; i++)
+// {
+//     /* code */
+//     cout<<i<<endl;
+//     if(i==2){
+//         break;
+//     }
+int main(){
+// for (int i = 0; i < 40; i++)
+// {
+//     /* code */
+//     if(i==2){
+//         continue;
+//     }
+//     cout<<i<<endl;
+
+return 0;
+}
