b0c02b3d65aa1c9aae14f2d4bda8bc06c0d771b3
Muzamilkamran <muzamilkamran3@gmail.com>
2022-07-01 07:06:56 -0700

    array&pointer

---
+++ tut14.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+ int marks[]={34,27,45,54};
+//  int mathmarks[4];
+//  mathmarks[0] = 783;
+//  mathmarks[1] = 782;
+//  mathmarks[2] = 718;
+//  mathmarks[3] = 781;
+//  cout<<"these are Maths marks"<<endl;
+//  cout<<mathmarks[0]<<endl;
+//  cout<<mathmarks[1]<<endl;
+//  cout<<mathmarks[2]<<endl;
+//  cout<<maththmarks[3]<<endl;
+ cout<<marks[0]<<endl;
+ cout<<marks[1]<<endl;
+ cout<<marks[2]<<endl;
+ cout<<marks[3]<<endl;
+return 0;
+}
