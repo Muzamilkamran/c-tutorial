672320747749a12604bfa3c4b623b2c91fb31710
Muzamilkamran <muzamilkamran3@gmail.com>
2022-07-03 10:22:33 -0700

    struct,union, enum

---
+++ tut15.cpp
@@ -0,0 +1,42 @@
+// #include<iostream>
+// using namespace std;
+
+// struct employee
+// {
+//     /* data */
+//     int eId;
+//     char favchar;
+//     float salary;
+
+//  };
+ 
+//  union money
+// {
+//     /* data */
+//     int rich;
+//     char car;
+//     float pounds;
+
+// };
+
+// int main(){
+// enum Meal{ breakfast, lunch, diner};
+// Meal m1 = breakfast;
+// cout<<m1;
+// cout<<lunch;
+// cout<<diner;
+
+
+//     // union money m1;
+//     // m1.rich =   34;
+//     // m1.car = 'c';
+//     // cout<<m1.rich;
+// // struct employee harry;
+// // harry.eId = 1;
+// // harry.favchar = 'c';
+// // harry.salary = 200000;
+// // cout<<"The value is "<<harry.eId<<endl;
+// // cout<<"The value is "<<harry.favchar <<endl;
+// // cout<<"The value is "<<harry.salary<<endl;
+// // return 0;
+// }
