6db2484a8f25f8dcd404f2843a6fd1d117a8ddd0
Muzamilkamran <muzamilkamran3@gmail.com>
2022-07-14 05:23:56 -0700

    Function Overloading with Examples in C++ | C++

---
+++ tut21.cpp
@@ -0,0 +1,39 @@
+Object Oriented Programming in C++
+Why oops?
+c++ language was desinged with the of adding object-0riented features to c language.
+As the size of programing increases,maintainability,readability and bug free nature
+of programs decreases.
+This was major problem with languages like c which relied upon function or procedures
+(hence the name procedural programind language)
+As  a result,the possibility   of not addresing the problem in an effective maner was high.
+Also, as data was most neglected data security was easily promised.
+Using a class solve the problem by modeling as a real world scenario.
+
+procedures oriented programing 
+
+
+consists of writting set of intructions for the computer to follow
+main focus is on function not on folw of data.
+function can either use local and global data.
+Data move openly from function to function.
+
+OBJECTED ORIENTED PROGRAMING
+Work on the concept of class and objects
+Treats data as critical element
+Decomposes the program in object and bulid data and function around the objects
+
+BASIC CONSPECTS IN OBJECT ORIENTED PROGRAMING
+
+CLASS--BASIC TEMPLE for creating objects.
+OBject--basic run time enities.
+Data Absrtuction &Encapsulation----wraping data and function into single unit.
+Inheritance----propresties of the one class can be inheritan into other.
+Polymorphism---Ability to take more than forms.
+Dynamic Binding----Code which will excute is not known untill the program run.
+Message Passing----Object message(information)  cal format.
+
+BENEFITS OF OBJECT ORIENTED PROGRAMING 
+
+Better code reusability use the object and inhertaanace.
+Principle of data hiding help bulid secure systems.
+Multiple objects can co.exit without interference.
